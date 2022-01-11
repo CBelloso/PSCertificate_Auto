fff9dc0058cbf625b3c679702ca0ba69dc477e41
CBelloso <bs13003@ues.edu.sv>
2022-01-11 17:28:16 -0600

    Python Photoshop

--- venv/main_v.py
+++
@@ -1,8 +0,0 @@
-#Abriendo Photoshop
-import win32com.client 
-import os 
-
-#psApp = win32com.client.Dispatch("Photoshop.Application")
-
-#C:\Users\mauri\Documents\VSCode\PythonPSprint("HolaMundo")
-
